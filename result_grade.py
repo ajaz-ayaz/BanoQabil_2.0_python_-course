d5b0980577d126feabd08bb76b6b46490b3b3f32
ajaz-ayaz <156538611+ajaz-ayaz@users.noreply.github.com>
2024-02-09 22:54:22 +0500

    Add files via upload

---
+++ result_grade.py
@@ -0,0 +1,31 @@
+
+#    MUHAMMAD AJAZ AYAZ
+#      STUDENT ID: "63410"
+#      EMAIL  :  ejazayaz860@gmail.com
+
+print("EXAM GRADE RESULT")
+
+ #  PROGRAM WILL BE START  
+
+
+marks = int (input('ENTER YOUR MARKS...'))
+if marks >= 80 :
+    print("\n  WELDONE YOUR GRADE IS  A+")
+
+elif marks >= 70 :
+    print("\n GOOD YOUR GrADE IS A")
+
+elif marks >= 60 :
+    print(" \n NICE YOUR GRADE IS B")
+
+elif marks >= 50 :
+    print("\n WORK HARD YOUR GRADE IS C ")
+
+elif marks >= 40 :
+    print("\n BETTER EFFORD NEXT TIME YOUR Grade IS D")
+
+elif marks >= 33 :
+    print("\n YPUR GRADE IS  E")
+
+else :
+    print('You are fail ')
